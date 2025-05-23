965259e368adbad6bef0976dd4150b1c558ba954
yanaibag <yanaialbagk@gmail.com>
2025-05-23 09:07:59 +0300

    added nodejs

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app =  express();
+const PORT = 1608;
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, "style")));
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+app.get('/login.html', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'))
+});
+
+app.post('/login.html', (req, res) => {
+    console.log(req.body);
+});
+
+app.get('/contact.html', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'))
+});
+
+app.post('/contact.html', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`)
+});
